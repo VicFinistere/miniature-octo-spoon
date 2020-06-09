8a7ce81f3fddd695e713ff2edffe2279ced55914
Zoé Belleton <zoebelleton@gmail.com>
2020-06-09 23:18:03 +0200

    Now there is autocompletion

---
+++ cmake-build-debug/CMakeFiles/SDLGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLGame.dir/src/main.cpp.o"
+  "bin/SDLGame"
+  "bin/SDLGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
